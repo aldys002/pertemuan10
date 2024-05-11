81e74c9a402c9a582eb538aad7453b2980e30ab2
aldys002 <igidiaaldys@gmail.com>
2024-05-11 10:17:27 +0700

    menambah return 0

--- stack.cpp
+++ stack.cpp
@@ -100,7 +100,15 @@ int main(){
             }
             break;
         case 4 : 
-        
+            cout << "Exiting program." << endl;
+            break;
+        default:
+            cout << "Invalid choice. Try again." << endl;
+            break;
        } 
+
+         cout << endl;
     }
+
+      return 0;
 }
